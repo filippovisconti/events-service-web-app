a8e320383b12c55103e17664cec21ee92eb504bb
Filippo Visconti <filippo.visconti23@gmail.com>
2022-06-04 18:08:15 +0200

    first upload

---
+++ src/main/java/it/uniroma3/siw/siweventsservice/SiwEventsServiceApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siweventsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwEventsServiceApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(SiwEventsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siweventsservice/SiwEventsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siweventsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwEventsServiceApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
